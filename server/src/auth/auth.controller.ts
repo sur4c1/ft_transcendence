e4c01c719d47920adbdc06845acd30b1f89fa77f
sur4c1 <94838879+sur4c1@users.noreply.github.com>
2023-06-13 11:45:10 +0200

    init JWT branch

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,129 +1,128 @@
-import { Body, Controller, Get, HttpException, HttpStatus, Post, Query, Session, UseGuards } from "@nestjs/common";
-import { ClearanceGuard } from "src/guards/clearance.guard";
-import { AuthService } from "./auth.service";
-import { UserService } from "../user/user.service";
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Post,
+  Query,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { ClearanceGuard } from 'src/guards/clearance.guard';
+import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
 
 @Controller('auth')
 export class AuthController {
-	constructor(
-		private authService: AuthService,
-		private userService: UserService
-	) { }
-	
-	/**
-	 * @brief Login with intra
-	 * @param {string} code Code given by intra
-	 * @param {Record<string, any>} session Session (redis)
-	 * @returns { {
-	 * 				status: 'logged' | 'registered',
-	 * 				needTo2FA: boolean
-	 * 		} } Status of the login and if the user enabled 2FA
-	 * @security Anyone
-	 * @response 200 - OK
-	 * @response 400 - Bad request
-	 * @response 500 - Internal server error
-	 */
-	@Get('login')
-	@UseGuards(new ClearanceGuard(Number(process.env.ADMIN_CLEARANCE)))
-	async intraLogin(
-		@Query('code') code: string,
-		@Session() session: Record<string, any>
-	): Promise<{
-		status: string,
-		needTo2FA: boolean
-	}>
-	{
-		let status = 'logged'
-		let intraUser = await this.authService.getIntraUser(code);
-		if (!intraUser)
-			throw new HttpException('Invalid code', HttpStatus.BAD_REQUEST);
-		let user = await this.userService.findByLogin(intraUser.login);
-		if (!user)
-		{
-			status = 'registered';
-			let name = intraUser.usual_first_name 
-				? intraUser.usual_first_name
-				: intraUser.first_name;
-			let i = 1;
-			while (await this.userService.findByName(name + (i == 1 ? '' : i)))
-				i++;
-			user = await this.userService.create({
-				login: intraUser.login,
-				name: name + (i == 1 ? '' : i),
-				clearance: Number(process.env.USER_CLEARANCE),
-				/* avatar: default_avatar */
-			});
-		}
-		session.userLogin = user.dataValues.login;
-		session.needToA2F = user.dataValues.has2FA;
-		if (!user.dataValues.hasConnected)
-			await this.userService.update({login: user.dataValues.login, hasConnected: true});
-		// console.log(session)
-		return {
-			status: status,
-			needTo2FA: user.dataValues.has2FA
-		};
-	}
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
 
-	/**
-	 * @brief Logout
-	 * @param {Record<string, any>} session Session (redis)
-	 * @security Clearance user
-	 * @response 200 - OK
-	 * @response 500 - Internal server error
-	 */
-	@Get('logout')
-	@UseGuards(new ClearanceGuard(Number(process.env.USER_CLEARANCE)))
-	async intraLogout(
-		@Session() session: Record<string, any>
-	): Promise<void>
-	{
-		try {
-			return session.destroy();
-		} catch (error) {
-			throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
+  /**
+   * @brief Login with intra
+   * @param {string} code Code given by intra
+   * @param {Record<string, any>} session Session (redis)
+   * @returns { {
+   * 				status: 'logged' | 'registered',
+   * 				needTo2FA: boolean
+   * 		} } Status of the login and if the user enabled 2FA
+   * @security Anyone
+   * @response 200 - OK
+   * @response 400 - Bad request
+   * @response 500 - Internal server error
+   */
+  @Get('login')
+  @UseGuards(new ClearanceGuard(Number(process.env.ADMIN_CLEARANCE)))
+  async intraLogin(
+    @Query('code') code: string,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<{
+    status: string;
+    needTo2FA: boolean;
+  }> {
+    let status = 'logged';
+    let intraUser = await this.authService.getIntraUser(code);
+    if (!intraUser)
+      throw new HttpException('Invalid code', HttpStatus.BAD_REQUEST);
+    let user = await this.userService.findByLogin(intraUser.login);
+    if (!user) {
+      status = 'registered';
+      let name = intraUser.usual_first_name
+        ? intraUser.usual_first_name
+        : intraUser.first_name;
+      let i = 1;
+      while (await this.userService.findByName(name + (i == 1 ? '' : i))) i++;
+      user = await this.userService.create({
+        login: intraUser.login,
+        name: name + (i == 1 ? '' : i),
+        clearance: Number(process.env.USER_CLEARANCE),
+        /* avatar: default_avatar */
+      });
+    }
+    if (!user.dataValues.hasConnected)
+      await this.userService.update({
+        login: user.dataValues.login,
+        hasConnected: true,
+      });
+    res.cookie('userLogin', user.dataValues.login, {
+      maxAge: 42 * 60 * 1000, // 42 minutes
+      httpOnly: false,
+    });
+    return {
+      status: status,
+      needTo2FA: user.dataValues.has2FA,
+    };
+  }
 
-	@Get('clearance')	
-	async getClearance(
-		@Session() session: Record<string, any>
-	): Promise<number>
-	{
-		let user = await this.userService.findByLogin(session.userLogin);
-		if (!user)
-			throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-		return user.dataValues.clearance;
-	}
-	
-	/**
-	 * @brief verify the TOTP code
-	 * @param {number} TOTP TOTP code
-	 * @param {Record<string, any>} session Session (redis)
-	 * @returns { {
-	 * 				status: 'logged'
-	 * 		} } Status of the login
-	 * @security Clearance user
-	 * @response 200 - OK
-	 * @response 400 - Bad request
-	 * @response 404 - Not found
-	 * @response 500 - Internal server error
-	 */
-	@Post('A2F')
-	@UseGuards(new ClearanceGuard(Number(process.env.USER_CLEARANCE)))
-	async verifyA2F(
-		@Body('TOTP') TOTP: number,
-		@Session() session: Record<string, any>
-	): Promise<{ status: 'logged' }>
-	{
-		let user = await this.userService.findByLogin(session.userLogin);
-		if (!user)
-			throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-		if (!await this.authService.verifyTOTP(TOTP, user.dataValues.A2FSecret))
-			throw new HttpException('Invalid TOTP', HttpStatus.BAD_REQUEST);
-		session.needToA2F = false;
-		return {
-			status: 'logged'
-		};
-	}
+  /**
+   * @brief Logout
+   * @param {Record<string, any>} session Session (redis)
+   * @security Clearance user
+   * @response 200 - OK
+   * @response 500 - Internal server error
+   */
+  @Get('logout')
+  @UseGuards(new ClearanceGuard(Number(process.env.USER_CLEARANCE)))
+  async intraLogout(@Res({ passthrough: true }) res: Response): Promise<void> {
+    res.clearCookie('userLogin');
+  }
+
+  @Get('clearance')
+  async getClearance(@Req() req: Request): Promise<number> {
+    let user = await this.userService.findByLogin(session.userLogin);
+    if (!user) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    return user.dataValues.clearance;
+  }
+
+  /**
+   * @brief verify the TOTP code
+   * @param {number} TOTP TOTP code
+   * @param {Record<string, any>} session Session (redis)
+   * @returns { {
+   * 				status: 'logged'
+   * 		} } Status of the login
+   * @security Clearance user
+   * @response 200 - OK
+   * @response 400 - Bad request
+   * @response 404 - Not found
+   * @response 500 - Internal server error
+   */
+  @Post('A2F')
+  @UseGuards(new ClearanceGuard(Number(process.env.USER_CLEARANCE)))
+  async verifyA2F(
+    @Body('TOTP') TOTP: number,
+    @Session() session: Record<string, any>,
+  ): Promise<{ status: 'logged' }> {
+    let user = await this.userService.findByLogin(session.userLogin);
+    if (!user) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    if (!(await this.authService.verifyTOTP(TOTP, user.dataValues.A2FSecret)))
+      throw new HttpException('Invalid TOTP', HttpStatus.BAD_REQUEST);
+    session.needToA2F = false;
+    return {
+      status: 'logged',
+    };
+  }
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,27 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
-var session = require('express-session');
+import cookieParser from 'cookie-parser';
+// var session = require('express-session');
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-	app.use(
-		helmet(),
-		session({
-			secret: process.env.SESSION_SECRET,
-			resave: true,
-			httpOnly: false,
-			saveUninitialized: true,
-			maxAge: 42 * 60 * 1000, // 42 minutes
-			name: 'xinjingping',
-		})
-	)
-	app.enableCors({
-		origin: process.env.FRONTEND_URL,
-		credentials: true,
-	});
-	app.setGlobalPrefix('api');
-	await app.listen(process.env.PORT);
+  app.use(
+    helmet(),
+    // session({
+    //   secret: process.env.SESSION_SECRET,
+    //   resave: true,
+    //   httpOnly: false,
+    //   saveUninitialized: true,
+    //   maxAge: 42 * 60 * 1000, // 42 minutes
+    //   name: 'xinjingping',
+    // }),
+    cookieParser(),
+  );
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  });
+  app.setGlobalPrefix('api');
+  await app.listen(process.env.PORT);
 }
 bootstrap();
