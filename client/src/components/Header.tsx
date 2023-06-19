bdb953c786b3af2f2474b3c3af57cde48f572a72
sur4c1 <94838879+sur4c1@users.noreply.github.com>
2023-06-19 10:30:52 +0200

    begin styling header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,28 +1,34 @@
 import { Link } from "react-router-dom";
 import Auth from "./Auth";
+import logo from "../assets/placeholder_logo.png"; // TODO: Replace with actual logo
+import style from "../style/Header.module.scss";
 
 const Header = () => {
 	return (
-		<>
-			<h1>Header</h1>
-			<ul>
-				<li>
-					<ul>
-						<li>
-							<Link to='/'>Home</Link>
-						</li>
-						<li>Playfield</li>
-					</ul>
-				</li>
-				<li>
-					<ul>
-						<li>
-							<Auth />
-						</li>
-					</ul>
-				</li>
-			</ul>
-		</>
+		<ul className={style.banner}>
+			<li>
+				<ul>
+					<li>
+						<Link to='/'>Home</Link>
+					</li>
+					{/* <li>Playfield</li> */}
+				</ul>
+			</li>
+			<li>
+				<ul>
+					<li>
+						<img src={logo} alt='logo' />
+					</li>
+				</ul>
+			</li>
+			<li>
+				<ul>
+					<li>
+						<Auth />
+					</li>
+				</ul>
+			</li>
+		</ul>
 	);
 };
 
