9b8a38361a59e470fb57debf47b57d53647b2798
sur4c1 <94838879+sur4c1@users.noreply.github.com>
2023-06-21 12:38:02 +0200

    Paddle + ball starting position

--- client/src/components/Game.tsx
+++ client/src/components/Game.tsx
@@ -2,59 +2,89 @@ import Matter from "matter-js";
 import { useEffect } from "react";
 
 const Game = () => {
+	const HEIGHT = 600;
+	const WIDTH = 800;
+	const PADDLE_HEIGHT = 30;
+	const PADDLE_WIDTH = 8;
+	const BALL_SIZE = 8;
+	const PADDLE_DISTANCE_FROM_EDGE = 121;
+	const BALL_STARTING_Y_VELOCITY = 6;
 	// module aliases
 	useEffect(() => {
 		var Engine = Matter.Engine,
 			Render = Matter.Render,
 			Runner = Matter.Runner,
 			Bodies = Matter.Bodies,
-			Composite = Matter.Composite;
+			Composite = Matter.Composite,
+			Events = Matter.Events;
+
+		let ballVelocity = {
+			x: 3,
+			y: BALL_STARTING_Y_VELOCITY,
+		};
+		let ballPosition = {
+			x: WIDTH / 2 - BALL_SIZE / 2,
+			y: 0,
+		};
+		let playerBatPosition = (PADDLE_HEIGHT + HEIGHT) / 2;
+		let adversBatPosition = (PADDLE_HEIGHT + HEIGHT) / 2;
 
 		// create an engine
-		var engine = Engine.create();
+		var engine = Engine.create({
+			gravity: {
+				x: 0,
+				y: 0,
+			},
+		});
 
 		// create a renderer
 		var render = Render.create({
 			element: document.body,
 			engine: engine,
+			options: {
+				width: WIDTH,
+				height: HEIGHT,
+			},
 		});
-
-		var mouse = Matter.Mouse.create(render.canvas),
-			mouseConstraint = Matter.MouseConstraint.create(engine, {
-				mouse: mouse,
-				constraint: {
-					stiffness: 0.2,
-					render: {
-						visible: false,
-					},
-				},
-			});
-		render.mouse = mouse;
 
 		// create two boxes and a ground
-		var playerBat = Bodies.rectangle(400, 200, 25, 200, {
-			isStatic: true,
-		});
-		var adversBat = Bodies.rectangle(450, 50, 25, 200, {
-			isStatic: true,
-		});
-		var ball = Bodies.circle(450, 50, 20, {
-			isStatic: true,
-		});
-		var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-		var ceiling = Bodies.rectangle(400, 0, 810, 60, { isStatic: true });
-		var leftWall = Bodies.rectangle(0, 300, 60, 610, { isStatic: true });
-		var rightWall = Bodies.rectangle(800, 300, 60, 610, { isStatic: true });
+		var playerBat = Bodies.rectangle(
+			PADDLE_DISTANCE_FROM_EDGE,
+			playerBatPosition,
+			PADDLE_WIDTH,
+			PADDLE_HEIGHT
+		);
+		var adversBat = Bodies.rectangle(
+			WIDTH - PADDLE_DISTANCE_FROM_EDGE - PADDLE_WIDTH,
+			adversBatPosition,
+			PADDLE_WIDTH,
+			PADDLE_HEIGHT
+		);
+		var ball = Bodies.rectangle(
+			ballPosition.x,
+			ballPosition.y,
+			BALL_SIZE,
+			BALL_SIZE,
+			{
+				friction: 0,
+				frictionAir: 0,
+			}
+		);
+		Matter.Body.setVelocity(ball, ballVelocity);
+		// var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+		// var ceiling = Bodies.rectangle(400, 0, 810, 60, { isStatic: true });
+		// var leftWall = Bodies.rectangle(0, 300, 60, 610, { isStatic: true });
+		// var rightWall = Bodies.rectangle(800, 300, 60, 610, { isStatic: true });
 
 		// add all of the bodies to the world
 		Composite.add(engine.world, [
 			playerBat,
 			adversBat,
 			ball,
-			ground,
-			ceiling,
-			leftWall,
-			rightWall,
+			// ground,
+			// ceiling,
+			// leftWall,
+			// rightWall,
 		]);
 
 		// run the renderer
