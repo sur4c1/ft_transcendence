cd67cbe10b27a4fd0d3a62f2a54a7d8c06770a02
Firencio <124384123+Firencio@users.noreply.github.com>
2023-10-05 19:01:48 +0200

    Add 404 & 401 Page

--- client/src/components/ThereIsNoFuckingPageBro.tsx
+++ client/src/components/ThereIsNoFuckingPageBro.tsx
@@ -1,12 +1,14 @@
+import style from "../style/App.module.scss";
+
 const ThereIsNoFuckingPageBro = () => {
-	/**
+	/*
 	 * 404 Page
 	 */
 	return (
 		<>
-			<div>
-				<h1>404</h1>
-				<p>No u</p>
+			<div className={style.error404}>
+				<img src="https://globalonlinepay.com/assets/images/feature/404.png"></img>
+				<pre>P I N G ... B U T ... W H E R E  I S  P O N G  ?</pre>
 			</div>
 		</>
 	);
--- client/src/components/ThereIsNotEnoughPermsBro.tsx
+++ client/src/components/ThereIsNotEnoughPermsBro.tsx
@@ -1,12 +1,14 @@
+import style from "../style/App.module.scss";
+
 const ThereIsNotEnoughPermsBro = () => {
 	/**
 	 * 401/403 Page
 	 */
 	return (
 		<>
-			<div>
-				<h1>401</h1>
-				<p>Not 4 u</p>
+			<div className={style.errorpage}>
+				<img src="https://cdn-bippi.nitrocdn.com/bqwDxSPDRpDwmYRiWNYPrkliEvinJKIP/assets/images/optimized/rev-a9389c0/privateproxy.me/wp-content/uploads/2022/04/810x348-What-is-a-Proxy-Error.png"></img>
+				<pre>A U T H O R I Z A T I O N   R E Q U I R E D</pre>
 			</div>
 		</>
 	);
