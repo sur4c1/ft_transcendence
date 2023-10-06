c67e955d97482050b36bd58d35e77eba9655b48b
sur4c1 <94838879+sur4c1@users.noreply.github.com>
2023-10-06 09:16:21 +0200

    add symbols in powerups

--- client/src/components/GameRender/drawMovables.tsx
+++ client/src/components/GameRender/drawMovables.tsx
@@ -5,13 +5,28 @@ export default function drawMovables(p5: p5Types, movables: Movable[]) {
 	p5.push();
 	for (let object of movables) {
 		p5.fill(object.color);
-		if (object.type === "circle")
+		if (object.type === "circle") {
+			p5.stroke("#ffffff");
+			p5.strokeWeight(5);
 			p5.circle(
 				object.position.x,
 				object.position.y,
 				object.size.radius * 2
 			);
-		else if (object.type === "rectangle")
+			p5.textSize(10);
+			p5.text(
+				object.color === "green"
+					? "↑"
+					: object.color === "red"
+					? "↓"
+					: "o",
+				object.position.x,
+				object.position.y,
+				object.size.radius * 2,
+				object.size.radius * 2
+			);
+			p5.textAlign(p5.CENTER, p5.CENTER);
+		} else if (object.type === "rectangle")
 			p5.rect(
 				object.position.x,
 				object.position.y,
