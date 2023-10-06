e44cc604b1cc480c962a0d52b2d7b142ec07a12c
sur4c1 <94838879+sur4c1@users.noreply.github.com>
2023-10-06 09:52:13 +0200

    honestly its a long time that will be okay

--- server/src/auth/jwt.service.ts
+++ server/src/auth/jwt.service.ts
@@ -4,6 +4,8 @@ import * as jwt from 'jsonwebtoken';
 @Injectable()
 export class JWTService {
 	async tokenise(payload: any) {
-		return jwt.sign(payload, process.env.JWT_KEY, { expiresIn: '365d' });
+		return jwt.sign(payload, process.env.JWT_KEY, {
+			expiresIn: 1000 * 60 * 60 * 24 * 7,
+		});
 	}
 }
